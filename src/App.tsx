82d558d092d4e3b81f07d27f0a7949132e91b6af
Ivoniak <amandaivoniak@gmail.com>
2022-04-10 18:58:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Botao from './components/buttons';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          rélo uord
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        ><Botao></Botao>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
